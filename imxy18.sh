9ef039f55e640340c7cfe28fd055b1255ba45df4
mike <mike@box29>
2017-04-01 16:28:14 +0800

    Add imxy18.sh script.

---
+++ imxy18.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+if [ "$#" -ne 2 ]; then
+    echo "$0 email name"
+    exit
+fi
+
+echo 0=$0
+echo 1=$1
+echo 2=$2
+
+sudo apt-get update && \
+sudo apt-get install -y gawk wget git-core diffstat unzip texinfo \
+        gcc-multilib build-essential chrpath socat libsdl1.2-dev \
+        libsdl1.2-dev xterm sed cvs subversion coreutils texi2html \
+        docbook-utils python-pysqlite2 help2man make gcc g++ \
+        desktop-file-utils libgl1-mesa-dev libglu1-mesa-dev \
+        mercurial autoconf automake groff curl lzop asciidoc \
+        u-boot-tools \
+        bc pv vim openssh-server tmux && \
+    cp /usr/share/zoneinfo/Asia/Taipei /etc/localtime && \
+    echo 'LANG="en_US.UTF-8"' > /etc/default/locale && \
+    echo 'Asia/Taipei' > /etc/timezone && \
+    locale-gen en_US.UTF-8 && \
+    dpkg-reconfigure -f non-interactive tzdata
+
+sudo wget https://commondatastorage.googleapis.com/git-repo-downloads/repo -O /usr/local/bin/repo
+sudo chmod 755 /usr/local/bin/repo
+sudo gpasswd -a mike sudo
+
+
+git config --global user.email $1
+git config --global user.name $2
+git config -l
