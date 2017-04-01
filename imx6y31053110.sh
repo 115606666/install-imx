42c4f413184a807e506c64d586ef6fa3bfc9e6bf
mike <mike@box29>
2017-04-01 16:54:22 +0800

    commit

--- imx6y31053110.sh
+++ imx6y31053110.sh
@@ -3,5 +3,4 @@
 mkdir yocto && cd yocto && \
 repo init \
     -u git://git.freescale.com/imx/fsl-arm-yocto-bsp.git -b imx-3.10.53-1.1.0_ga && \
-repo sync -c -j8 && \
-echo "!!!!!! docker-imx6y31053110 done."
+repo sync -c -j8
