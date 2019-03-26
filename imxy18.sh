2f34468f7516ad71d1353d2350b3a8df022db1be
mike <mike@box43>
2019-03-26 13:40:20 +0800

    commit

--- imxy18.sh
+++ imxy18.sh
@@ -11,6 +11,12 @@ echo 2=$2
 
 sudo apt-get update && \
 sudo apt-get -y dist-upgrade && \
+sudo apt-get -y install locales && \
+sudo cp /usr/share/zoneinfo/Asia/Taipei /etc/localtime && \
+echo 'LANG="en_US.UTF-8"' | sudo dd of=/etc/default/locale && \
+echo 'Asia/Taipei' | sudo dd of=/etc/timezone && \
+sudo locale-gen en_US.UTF-8 && \
+sudo dpkg-reconfigure -f non-interactive tzdata && \
 sudo apt-get -y install gawk wget git-core diffstat unzip texinfo \
         gcc-multilib build-essential chrpath socat libsdl1.2-dev \
         libsdl1.2-dev xterm sed cvs subversion coreutils texi2html \
@@ -18,12 +24,7 @@ sudo apt-get -y install gawk wget git-core diffstat unzip texinfo \
         desktop-file-utils libgl1-mesa-dev libglu1-mesa-dev \
         mercurial autoconf automake groff curl lzop asciidoc \
         u-boot-tools libncurses5-dev lib32ncursesw5-dev \
-        bc pv vim openssh-server tmux locales file && \
-sudo cp /usr/share/zoneinfo/Asia/Taipei /etc/localtime && \
-echo 'LANG="en_US.UTF-8"' | sudo dd of=/etc/default/locale && \
-echo 'Asia/Taipei' | sudo dd of=/etc/timezone && \
-sudo locale-gen en_US.UTF-8 && \
-sudo dpkg-reconfigure -f non-interactive tzdata
+        bc pv vim openssh-server tmux file
 
 sudo wget https://commondatastorage.googleapis.com/git-repo-downloads/repo -O /usr/local/bin/repo
 sudo chmod 755 /usr/local/bin/repo
