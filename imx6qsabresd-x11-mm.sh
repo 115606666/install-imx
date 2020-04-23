de84eff81a14a71aee9f100f561e2facac3cd688
mike <mike@box43>
2020-04-23 10:31:24 +0800

    commit

---
+++ imx6qsabresd-x11-mm.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+SEP_LINE=------------------------------------------------------------------
+LOG_FILENAME=
+START_TIME=
+DIFF_TIME=
+
+print_df_free()
+{
+    date +%Y%m%d%H%M%S | tee -a $LOG_FILENAME
+    df                 | tee -a $LOG_FILENAME
+    free               | tee -a $LOG_FILENAME
+}
+
+print_sep_line()
+{
+    echo $SEP_LINE | tee -a $LOG_FILENAME
+}
+
+get_start_time()
+{
+    local START_DATETIME=`date +%Y%m%d%H%M%S`
+    START_TIME=$(date +%s)
+    echo START_DATETIME=${START_DATETIME} | tee -a ${LOG_FILENAME}
+}
+
+print_diff_time()
+{
+    local END_DATETIME=$(date +%Y%m%d%H%M%S)
+    local END_TIME=$(date +%s)
+    local DIFF=$(echo "$END_TIME - $START_TIME" | bc)
+    local HH=$(echo "$DIFF / 3600" | bc)
+    local MM=$(echo "$DIFF - ($HH * 3600)" | bc)
+    local MM=$(echo "$MM / 60" | bc)
+    local SS=$(echo "$DIFF % 60" | bc)
+    DIFF_TIME=$HH:$MM:$SS
+    echo DIFF_TIME=$DIFF_TIME
+}
+
+    mkdir -p ~/log
+    BUILD_NAME=`basename $0 .sh`
+    LOG_FILENAME=~/log/build_${BUILD_NAME}_`date +%Y%m%d%H%M%S`.txt
+    print_df_free
+
+    MACHINE_NAME=imx6qsabresd
+    DISTRO_NAME=fsl-imx-x11
+    IMAGE_NAME=imx-image-multimedia
+    BUILD_PATH=build
+
+    echo MACHINE_NAME=${MACHINE_NAME} | tee -a ${LOG_FILENAME}
+    echo DISTRO_NAME=${DISTRO_NAME}   | tee -a ${LOG_FILENAME}
+    echo IMAGE_NAME=${IMAGE_NAME}     | tee -a ${LOG_FILENAME}
+    echo BUILD_PATH=${BUILD_PATH}     | tee -a ${LOG_FILENAME}
+
+    echo export MACHINE_NAME=${MACHINE_NAME} | tee -a ~/.bashrc
+    echo export DISTRO_NAME=${DISTRO_NAME}   | tee -a ~/.bashrc
+    echo export IMAGE_NAME=${IMAGE_NAME}     | tee -a ~/.bashrc
+    echo export BUILD_PATH=${BUILD_PATH}     | tee -a ~/.bashrc
+
+    cd ~/yocto
+
+    # Build yocto
+    print_sep_line
+    print_df_free
+    get_start_time
+    DISTRO=${DISTRO_NAME} MACHINE=${MACHINE_NAME} EULA=1 source ./fsl-setup-release.sh -b ${BUILD_PATH}
+    time bitbake ${IMAGE_NAME} 2>&1 | tee -a ${LOG_FILENAME}
+    print_diff_time
+    TIME1=$DIFF_TIME
+    ls -la tmp/deploy/images/* | tee -a ${LOG_FILENAME}
+    print_df_free
+
+    # build SDK
+    print_sep_line
+    print_df_free
+    get_start_time
+    time bitbake $IMAGE_NAME -c populate_sdk 2>&1 | tee -a $LOG_FILENAME
+    print_diff_time
+    TIME2=$DIFF_TIME
+    ls -la tmp/deploy/sdk | tee -a $LOG_FILENAME
+    print_df_free
+
+    # Print summary
+    print_sep_line
+    echo TIME1=$TIME1 | tee -a $LOG_FILENAME
+    echo TIME2=$TIME2 | tee -a $LOG_FILENAME
+
+    echo "!!!!!! ${BUILD_NAME} done."
---
+++ imxy27.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+# Tested on Ubuntu 18.04
+
+if [ "$#" -ne 2 ]; then
+    echo "$0 email name"
+    exit
+fi
+
+echo 0=$0
+echo 1=$1
+echo 2=$2
+
+# i.MX Yocto Project User's Guide, Rev. L4.19.35_1.1.0, 11/2019
+sudo apt-get update && \
+sudo apt-get -y dist-upgrade && \
+sudo apt-get -y install gawk wget git diffstat unzip texinfo gcc-multilib \
+        build-essential chrpath socat libsdl1.2-dev \
+        xterm sed cvs subversion coreutils texi2html \
+        docbook-utils python-pysqlite2 help2man make gcc g++ \
+        desktop-file-utils \
+        libgl1-mesa-dev libglu1-mesa-dev mercurial autoconf automake groff \
+        curl lzop asciidoc \
+        u-boot-tools \
+        libncurses5-dev bc pv vim openssh-server tmux && \
+sudo cp /usr/share/zoneinfo/Asia/Taipei /etc/localtime && \
+echo 'LANG="en_US.UTF-8"' | sudo dd of=/etc/default/locale && \
+echo 'Asia/Taipei' | sudo dd of=/etc/timezone && \
+sudo locale-gen en_US.UTF-8 && \
+sudo dpkg-reconfigure -f non-interactive tzdata
+
+sudo wget https://commondatastorage.googleapis.com/git-repo-downloads/repo -O /usr/local/bin/repo
+sudo chmod 755 /usr/local/bin/repo
+sudo gpasswd -a mike sudo
+
+git config --global user.email $1
+git config --global user.name $2
+git config --global color.ui auto
+git config -l
+
+echo export LC_ALL="en_US.UTF-8" >> ~/.bashrc
